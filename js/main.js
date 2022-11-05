cf42337abddab4d6ac59810efcd2bd3cbee7a3b4
Franklin Protazio <protaziolancamentos@gmail.com>
2022-11-04 22:29:49 -0300

    Aulas JavaScript

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*
+var nome = "Franklin Protazio";
+var idade =  29;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2)
+console.log(frase.replace("Japão", "Brasil"));
+*/
+
+function trocar(elemento) {
+    elemento.innerHTML = "Obrigado"
+}
+
+function voltar(elemento) {
+    elemento.innerHTML ="Passou"
+}
